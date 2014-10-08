3a0b6c8c7d0d880cd3cf80db6076a375f26c4447
Thomas B <tbonin0328@yahoo.com>
2014-10-07 23:04:40 -0700

    still having trouble with divs

--- play.php
+++ play.php
@@ -15,19 +15,15 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF'])); //this is the environment va
 Enter your birth name (as it appears on your birth certificate) and your birth date. Then, click "Go!" to get your personal numbers analysis!
 </p>
 
-<p>
 <?php
 
 $alphas = range('A', 'Z');
 $hebChalNums = array(1,2,3,4,5,8,3,5,1,1,2,3,4,5,7,8,1,2,3,4,6,6,6,5,1,7);
 
-echo '<pre>';
-print_r($alphas);
-echo '</pre>';
+$numArrayFirst = array();
+$numArrayMiddle = array();
+$numArrayLast = array();
 
-echo '<pre>';
-print_r($hebChalNums);
-echo '</pre>';
 
 if(isset($_POST['FirstName'])) //_POST is a superglobal
 //always want to use _POST on forms
@@ -39,78 +35,105 @@ if(isset($_POST['FirstName'])) //_POST is a superglobal
 	$arrFirst = str_split($strFirst);
 	$arrMiddle = str_split($strMiddle);
 	$arrLast = str_split($strLast);
-	
-echo '<table cellpadding="7" cellspacing="0"> <tr> <td>';	
+
+echo '<div id = "numbers1">';
+echo 'Name: </ br>';
+echo 'Numerology </ br>';
+echo 'Consonsants </ br>';
+echo 'Vowels </ br> </div>';
+
+echo '<div id="numbers2">';	
+echo '<table class="main"> <tr> <td>';	
 //table for first name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrFirst as $letter)
 	{
-	    echo '<td align="center">' . $letter . '</td>';    
+	    echo '<td>' . $letter . '</td>';    
 	}
 
-    echo '</table> </td> <td>';
+   	echo '</table></td><td>';
 
 //table for middle name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrMiddle as $letter)
 	{
-	    echo '<td align="center">' . $letter . '</td>';    
+	    echo '<td>' . $letter . '</td>';    
 	}
 
     echo '</table></td><td>';
 	
 //table for last name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrLast as $letter)
 	{
-	    echo '<td align="center">' . $letter . '</td>';    
+	    echo '<td>' . $letter . '</td>';    
 	}
 
-    echo '</table></td></table>';
+    echo '</table></td></tr></table>';
 	
 	echo '</br />';
 	
-	echo '<table cellpadding="7" cellspacing="0"> <tr> <td>';	
+//assigned numbers tables start here
+	echo '<table class="main"><tr> <td>';
+	
 //table for first name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrFirst as $letter)
 	{
 	    $numIndex = array_search($letter, $alphas); 
-	    
-	    echo '<td align="center">' . $hebChalNums[$numIndex] . '</td>';    
+	    $numArrayFirst[] = $hebChalNums[$numIndex];
+	    echo '<td>' . $hebChalNums[$numIndex] . '</td>';    
 	}
 
-    echo '</table> </td> <td>';
+    echo '</table></td><td>';
 
 //table for middle name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrMiddle as $letter)
 	{
-	    echo '<td align="center">' . $letter . '</td>';    
+	    $numIndex = array_search($letter, $alphas); 
+		$numArrayMiddle[] = $hebChalNums[$numIndex];
+	    echo '<td>' . $hebChalNums[$numIndex] . '</td>';      
 	}
 
     echo '</table></td><td>';
 	
 //table for last name starts here	
-	echo '<table border="1" cellpadding="7" cellspacing="0" style="border-collapse:collapse;">';
+	echo '<table class="inside">';
 	echo "<tr>";
 
 	foreach ($arrLast as $letter)
 	{
-	    echo '<td align="center">' . $letter . '</td>';    
+	    $numIndex = array_search($letter, $alphas);
+		$numArrayLast[] = $hebChalNums[$numIndex];
+	    echo '<td>' . $hebChalNums[$numIndex] . '</td>';        
 	}
 
-    echo '</table></td></table>';
+    echo '</table></td></tr></table></div>';
+	
+	echo '<div id="numbers3">';
+	
+	echo '<table class="inside">';
+	echo "<tr>";
+
+	    echo '<td>' . array_sum($numArrayFirst) . '</td>';        
+		echo '<td>' . array_sum($numArrayMiddle) . '</td>';    
+		echo '<td>' . array_sum($numArrayLast) . '</td>';    
+	
+    echo '</table></td></tr></table></div>';
+	
+	
+	echo '<div="calcsleft">';
 	
 	
 	echo '</br />';
@@ -119,10 +142,11 @@ echo '<table cellpadding="7" cellspacing="0"> <tr> <td>';
 	echo "Your name is " . $_POST['FirstName'] . " ";
 	echo $_POST['MiddleName'] . " ";
 	echo $_POST['LastName'] . "."?> <br />
-
+	
+	
 <?php
 	echo "You were born on " . $_POST['BirthDate'] . " ";
-	echo '<br /><a href="' . THIS_PAGE . '">Reset</a>';
+	echo '<br /><a href="' . THIS_PAGE . '">Reset</a> </div></div>';
 }else {//show form; see closing bracket below form; that's how this works.
 
 ?>
@@ -138,6 +162,5 @@ Birth Date: <input type="date" name="BirthDate" /> <br /> <!--this last thing is
 <?php 
 }
 ?>
-</p>
 
 <?php include 'footer.php'; ?>
